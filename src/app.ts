5989e413a9558a9e5bd763346b1dd6f0c6ae0fbd
hoopra <hoopra@mail.com>
2019-08-29 07:35:07 +0200

    Migrate database before app listen

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import app from '@server';
 import { logger } from '@util';
+import { Express } from 'express';
 import cors from 'cors';
+import { database } from './database/pool';
 
-app.use(cors());
+const listen = async (app: Express) => {
+    app.use(cors());
 
-// Start the server
-const port = Number(process.env.PORT || 8000);
-app.listen(port, async () => {
-    logger.info('Express server started on port: ' + port);
-});
+    // Start the server
+    const port = Number(process.env.PORT || 8000);
+    // start database to force migration
+    await database();
+    app.listen(port, async () => {
+        logger.info('Express server started on port: ' + port);
+    });
+};
+
+listen(app);
